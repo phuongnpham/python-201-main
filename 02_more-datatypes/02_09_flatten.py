3e72491da4b505a5d3ff180c10b54bcbb464bf1e
phuongntpham <kris_pham@yahoo.com>
2025-03-27 18:44:09 -0700

    added 02_more-datatypes lab solution

--- 02_more-datatypes/02_09_flatten.py
+++ 02_more-datatypes/02_09_flatten.py
@@ -10,3 +10,22 @@
 # to flatten a list of any depth. Can you understand the code used?
 
 starter_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+flattened_list = [num for items in starter_list for num in items]
+print(flattened_list)
+
+# Flatten a list of any depth
+starter_list = [[1, 2, 3, 4], [[5, 6]], [[[7, 8, 9]]]]
+flattened_list = []
+while starter_list: # run until the given list is empty
+    item = starter_list.pop()
+    # print(item)
+    if type(item) == list: # check the type of the poped item
+        starter_list.extend(item) # if list extend the item to given list
+        # print(starter_list)
+        # print(flattened_list)
+    else:
+        flattened_list.append(item) # if not list then add it to the flat list
+        # print(starter_list)
+        # print(flattened_list)
+flattened_list.sort()
+print(flattened_list)
--- 02_more-datatypes/02_11_set_basics.py
+++ 02_more-datatypes/02_11_set_basics.py
@@ -4,3 +4,6 @@
 
 s = {1, 2, 3, 4}
 t = {3, 4, 5, 6}
+
+print(s.intersection(t))
+print(s.union(t))
