7141efaff218819b4c1c2d7fdb157a2a4d0a5502
phuongntpham <kris_pham@yahoo.com>
2025-04-05 11:10:07 -0700

    added 03_file-input-output lab solution

--- 03_file-input-output/03_01_long_words.py
+++ 03_file-input-output/03_01_long_words.py
@@ -1,2 +1,9 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+
+with open("03_file-input-output/words.txt", "r") as file_in:
+    for word in file_in:
+        word = word.strip()
+        if len(word) > 20:
+            print(word)
+file_in.close()
--- 03_file-input-output/03_02_words_analysis.py
+++ 03_file-input-output/03_02_words_analysis.py
@@ -4,3 +4,23 @@
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+word_dict = {}
+total_words = 0
+with open("03_file-input-output/words.txt", "r") as file_in:
+    for word in file_in:
+        total_words += 1
+        word = word.strip()
+        word_dict.update({word: len(word)})
+
+shortest_word = min(word_dict.values())
+longest_word = max(word_dict.values())
+for word in word_dict.keys():
+    if len(word) == shortest_word:
+        print(f"Shortest word: {word}")
+for word in word_dict.keys():
+    if len(word) == longest_word:
+        print(f"Longest word: {word}")
+print(f"Total number of words in file: {total_words}")
+
+file_in.close()
