3f080b211e33c38786c5baddddb9cd89fb313e61
phuongntpham <kris_pham@yahoo.com>
2025-04-06 16:09:55 -0700

    added 03_file-input-output lab solution

--- 03_file-input-output/file-counter/analyze.py
+++ 03_file-input-output/file-counter/analyze.py
@@ -1,1 +1,7 @@
 # Use the `csv` module to read in and count the different file types.
+import csv
+
+with open(("03_file-input-output/file-counter/filecounts.csv"), "r") as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames = ["Folder", "csv", "MD", "PNG"])
+    counts = list(reader)
+print(counts)
