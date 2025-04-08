780533241236eb7bfbab8b66549820284efe1476
phuongntpham <kris_pham@yahoo.com>
2025-04-07 22:00:02 -0700

    added 03_file-input-output lab solution

--- 03_file-input-output/file-counter/csvwrite.py
+++ 03_file-input-output/file-counter/csvwrite.py
@@ -1,1 +1,10 @@
 # Write the file counts to a `.csv` file.
+import json
+import csv
+
+with open(("03_file-input-output/file-counter/filecounts.json"), "r") as filecounts:
+    dict_contents = json.load(filecounts)
+
+with open(("03_file-input-output/file-counter/filecount.csv"), "w") as file_out:
+    writer = csv.writer(file_out)
+    writer.writerow(dict_contents.values())
