4bd2212e843f962c85684b697ec11faaead7bbf5
phuongntpham <kris_pham@yahoo.com>
2025-04-08 22:22:56 -0700

    added 03_file-input-output lab solution

--- 03_file-input-output/03_03_writing.py
+++ 03_file-input-output/03_03_writing.py
@@ -1,2 +1,7 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+with open("03_file-input-output/words.txt", "r") as file_in:
+    word = file_in.readlines()
+word_reversed = word[::-1]
+with open("03_file-input-output/words_reverse.txt", "w") as file_out:
+    file_out.writelines(word_reversed)
