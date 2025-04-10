88062fce9038cf6373762325f3b6ac8a1c54bdd5
phuongntpham <kris_pham@yahoo.com>
2025-04-09 21:50:07 -0700

    added 03_file-input-output lab solution

--- 03_file-input-output/03_04_pathlib_reader.py
+++ 03_file-input-output/03_04_pathlib_reader.py
@@ -1,3 +1,17 @@
 # Refactor your file counter script to use `pathlib` also for
 # reading and writing to your CSV file. Make sure to handle the
 # path in a way so that you can run the script from anywhere.
+import json
+import csv
+from pathlib import Path
+
+file_path = Path.cwd().joinpath("03_file-input-output/file-counter")
+json_path = file_path.joinpath("filecounts.json")
+csv_path = file_path.joinpath("filecount.csv")
+
+with open(json_path,"r") as json_file:
+    dict_contents = json.load(json_file)
+
+with open(csv_path, "w") as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(dict_contents.values())
--- 03_file-input-output/03_05_more-filetypes.py
+++ 03_file-input-output/03_05_more-filetypes.py
@@ -2,3 +2,37 @@
 # in your CSV file. Remember that the format of your output needs to be
 # consistent across multiple runs of your script. This means you'll need to
 # make a compromise and choose which file types you want to record beforehand.
+import csv
+import pprint
+from pathlib import Path
+
+desktop = Path.home().joinpath("Desktop")
+file_path = Path.cwd().joinpath("03_file-input-output")
+csv_file_path = file_path.joinpath("desktop_file_counts.csv")
+file_types = {
+    ".txt": 0,
+    ".jpg": 0,
+    ".ini": 0,
+    ".lnk": 0,
+    "no extension": 0
+}
+for f in desktop.iterdir():
+    if f.is_file():
+        if f.suffix:
+            extension = f.suffix.lower()
+        else:
+            extension = "no extension"
+
+        if extension in file_types:
+            file_types[extension] += 1
+        else:
+            file_types[extension] = 1
+
+print("File types on the Desktop:")
+pprint.pprint(file_types)
+
+with open(csv_file_path, "w") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Extension", "Count"])
+    for extension, count in file_types.items():
+        writer.writerow([extension, count])
