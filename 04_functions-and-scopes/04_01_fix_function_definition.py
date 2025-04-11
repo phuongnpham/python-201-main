d4420b8c238390ed626179ec5ff43a40dafd3716
phuongntpham <kris_pham@yahoo.com>
2025-04-10 22:10:54 -0700

    added 04_functions-and-scopes lab solution

--- 04_functions-and-scopes/04_01_fix_function_definition.py
+++ 04_functions-and-scopes/04_01_fix_function_definition.py
@@ -3,8 +3,8 @@
 # After your fixes, the function should allow you to take a name as an input
 # and return a greeting message that you can save to a variable.
 
-function say_hello("name"):
-        return print(f"Hello {name}!")
+def greet(name):
+        return (f"Hello {name}!")
 
-greeting = hello(name)
+greeting = greet("Tom")
 print(greeting)
