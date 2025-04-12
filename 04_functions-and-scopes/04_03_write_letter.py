c3a7b69f49486549e9b7fd264f7627a9f3a2c4b9
phuongntpham <kris_pham@yahoo.com>
2025-04-11 22:59:07 -0700

    added 04_functions-and-scopes lab solution

--- 04_functions-and-scopes/04_03_write_letter.py
+++ 04_functions-and-scopes/04_03_write_letter.py
@@ -3,3 +3,13 @@
 # message with the `name` input, as well as a goodbye message that uses
 # the `name` again. Combine that with the input `text` to return a
 # complete `letter`.
+def write_letter(name, text):
+    greeting = f"Dear {name},\n\n"
+    goodbye = f"\n\nBest regards,\n{name}"
+    letter = greeting + text + goodbye
+    return letter
+
+name = "John"
+text = "Hope you're doing well"
+letter = write_letter(name, text)
+print(letter)
--- 04_functions-and-scopes/04_04_combine_functions.py
+++ 04_functions-and-scopes/04_04_combine_functions.py
@@ -2,3 +2,17 @@
 # with the `write_letter()` function from the previous exercise.
 # Write both functions in this script and call `greet()` within `write_letter()`
 # to let `greet()` take care of creating the greeting string.
+
+def greet(name):
+    return f"Hello {name}!"
+
+def write_letter(name, text):
+    greeting = greet(name) + "\n\n"
+    good_bye = f"\n\nBest, \n{name}"
+    letter = greeting + text + good_bye
+    return letter
+
+name = "John"
+text = "How's it going? Let's catch up soon"
+letter = write_letter(name, text)
+print(letter)
