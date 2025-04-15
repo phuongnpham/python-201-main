0124bea24a02ccdf5177c3d00cc8c19f7909fc57
phuongntpham <kris_pham@yahoo.com>
2025-04-14 21:36:19 -0700

    added 04_functions-and-scopes lab solution

--- 04_functions-and-scopes/04_07_determine_divisible.py
+++ 04_functions-and-scopes/04_07_determine_divisible.py
@@ -7,3 +7,35 @@
 # - call your functions, passing in the user input as the arguments,
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
+def is_divisible_by_4_or_7(number):
+    """Check if the number is divisible by 4 OR 7
+
+    Args:
+        number (int): the number to check
+
+    Returns:
+        boolean: True if divisible by 4 OR 7, otherwise False
+    """
+    return number % 4 == 0 or number % 7 == 9
+
+def is_divisible_by_4_and_7(number):
+    """Check if the number is divisible by 4 AND 7
+
+    Args:
+        number (int): the number to check
+
+    Returns:
+        boolean: True if divisible by 4 AND 7, otherwise False
+    """
+    return number % 4 == 0 and number % 7 == 0
+
+user_input = int(input("Please enter a number between 1 and 1,000,000,000: "))
+
+if 1 <= user_input <= 1000000000:
+    or_check = is_divisible_by_4_or_7(user_input)
+    and_check = is_divisible_by_4_and_7(user_input)
+
+    print(f"Is {user_input} divisible by 4 or 7? {or_check}")
+    print(f"Is {user_input} divisible by 4 and 7? {and_check}")
+else:
+    print("Number must be in between 1 and 1,000,000,000")
--- 04_functions-and-scopes/04_08_calculate_stats.py
+++ 04_functions-and-scopes/04_08_calculate_stats.py
@@ -4,8 +4,16 @@
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
-  # define the function here
-  pass
+def stats(num_list):
+  max_number = max(num_list)
+  min_number = min(num_list)
+  sum_number = sum(num_list)
+  avg_number = sum_number / len(num_list)
+
+  print(f"Maximum number: {max_number}")
+  print(f"Minimum number: {min_number}")
+  print(f"Sum of the numbers: {sum_number}")
+  print(f"Average of the numbers: {avg_number}")
 
 # call the function below here
+stats(example_list)
