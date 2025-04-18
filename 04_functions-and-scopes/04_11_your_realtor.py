e2754b69eaf917ab5b57f0801897a2420886bd1f
phuongntpham <kris_pham@yahoo.com>
2025-04-17 19:44:01 -0700

    added 04_functions-and-scopes lab solution

--- 04_functions-and-scopes/04_11_your_realtor.py
+++ 04_functions-and-scopes/04_11_your_realtor.py
@@ -3,3 +3,33 @@
 # is why your function should be able to take an arbitrary amount of
 # keyword arguments, and display them all in a list form with some 
 # introductory information.
+
+def real_estate_ad(**kwargs):
+    print("New Real Estate Listing Information")
+    print("-" * 40)
+    for key, value in kwargs.items():
+        formatted_info = key.replace("_", " ").title()
+        print(f"{formatted_info}: {value}")
+    print("-" * 40)
+
+real_estate_ad(
+    home_type = "Single Family",
+    parking = "2 Garage Spaces",
+    bedroom = 3,
+    bathroom = 2,
+    price = 349900,
+    price_per_sqft = 209,
+    annual_tax = 174,
+    address = "622 Lemming Ln SE"
+)
+
+real_estate_ad(
+    home_type = "Mobile Manufactured",
+    bedroom = 4,
+    bathroom = 2,
+    heating = "Electric, Heat Pump",
+    cooling = "Central Air",
+    laundry = "Washer, Dryper",
+    price = 159900,
+    address = "2920 Brown Road SE"
+)
