5cb7ffa8e91f67fe9b6dfd4de2213c2f88cfc8d8
phuongntpham <kris_pham@yahoo.com>
2025-04-20 14:52:04 -0700

    added my_enumerate project solution

--- projects/my_enumerate.py
+++ projects/my_enumerate.py
@@ -2,6 +2,20 @@
 # Define a function called `my_enumerate()` that takes an iterable as input
 # and gives back both the element as well as its index position as an integer.
 
-def my_enumerate():  # add your arguments
-      # add your code implementation
-      pass
+def my_enumerate(iterable):
+    """Mimic the built-in enumerate() function
+
+    Args:
+        iterable: Any iterable input (e.g. list, tuple)
+    """
+    index = 0
+    result = []
+    for value in iterable:
+        result.append((index, value))
+        index += 1
+    return result
+
+courses = ['Intro', 'Intermediate', 'Advanced', 'Professional']
+
+for index, course in my_enumerate(courses):
+    print(f"{index}: {course} Python")
