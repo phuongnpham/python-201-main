4eab182dcac4ce2588f02ef3668ad79e40a6bb8e
phuongntpham <kris_pham@yahoo.com>
2025-05-02 22:05:43 -0700

    added 06_advanced-python-concepts lab solution

--- 06_advanced-python-concepts/06_12_lambda_sum.py
+++ 06_advanced-python-concepts/06_12_lambda_sum.py
@@ -1,2 +1,5 @@
 # Write a lambda expression that takes in three numbers
 # and returns the sum of the numbers.
+
+sum = lambda a, b, c: a + b + c
+print(sum(1, 2, 3))
--- 06_advanced-python-concepts/06_13_void_return.py
+++ 06_advanced-python-concepts/06_13_void_return.py
@@ -1,2 +1,5 @@
 # Write a lambda function that does not take in an arguments 
 # but returns a value. Print the return value.
+
+l = lambda: 5
+print(l())
